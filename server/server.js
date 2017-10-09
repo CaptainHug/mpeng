bc4ffc9212949dd46c2a4574137649d6b3e8847c
CaptainHug <jon_r_huggins@yahoo.co.uk>
2017-10-09 17:37:25 +0100

    Setting up the server framework.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 var config = require('./config.json');
 var logger = require('winston');
+var express = require("express");
+var http = require("http");
+var socketIo = require("socket.io");
 
-// Configure logger settings
+// Setup logger
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
 	colorize: true
@@ -9,3 +12,26 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 
 logger.info('mpeng starting up');
+
+// setup server
+var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
+
+io.on("connection", function(client) {
+	logger.info("Client connected");
+	
+	client.on("signup", function(data) {
+		logger.info("Client signup: data = " + data);
+	});
+	
+	client.on("login", function(data) {
+		logger.info("Client login: data = " + data);
+	});
+	
+	client.on("disconnect", function(data) {
+		logger.info("Client disconnected");
+	});
+});
+
+server.listen(config.server.port);
