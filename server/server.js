cb479b928e8586a76b33a320abf477b4f2e95dbb
CaptainHug <jon_r_huggins@yahoo.co.uk>
2017-10-10 14:40:59 +0100

    Made server compatible with FlashSocket.io

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 var config = require('./config.json');
 var logger = require('winston');
-var express = require("express");
-var http = require("http");
-var socketIo = require("socket.io");
+var io = require("socket.io");
 
 // Setup logger
 logger.remove(logger.transports.Console);
@@ -12,26 +10,25 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 
 // setup server
-var app = express();
-var server = http.createServer(app);
-var io = socketIo(server);
+var socket = io.listen(config.server.port);
+socket.configure(function() {
+	socket.set("transports", ["flashsocket", "htmlfile", "xhr-polling", "jsonp-polling"]);
+	socket.set('flash policy port', 843);
+	socket.set("log level", 2);
+});
 
-io.on("connection", function(client) {
-	logger.info("Client connected");
+socket.sockets.on("connection", function(client) {
+	logger.info("Client connected: " + client.id);
 	
-	client.on("signup", function(data) {
-		logger.info("Client signup: data = " + data);
-	});
-	
-	client.on("login", function(data) {
-		logger.info("Client login: data = " + data);
+	client.on("message", function(data) {
+		logger.info("Client message: data = " + JSON.stringify(data));
+		
+		client.emit("custom", {ball:"bag"});
 	});
 	
 	client.on("disconnect", function(data) {
 		logger.info("Client disconnected");
 	});
 });
-
-server.listen(config.server.port);
 
 logger.info("mpeng v" + config.server.version + " started up");
